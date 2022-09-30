e9cd3af442232870ef8f31e6af6f8d5a0c72ab6b
ShockTr <borakayik@icloud.com>
2022-10-01 00:42:18 +0300

    samsung fold support OMEGALUL

--- pages/tracks/index.tsx
+++ pages/tracks/index.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 const Tracks: NextPageWithLayout = ({ playlist }: InferGetStaticPropsType<typeof getStaticProps>) => {
     return (
         <div className="p-3">
-            <div className="text-white bg-slate-700/60 backdrop-blur shadow p-3 rounded grid gap-5 md:grid-cols-2 lg:grid-cols-5">
+            <div className="text-white bg-slate-700/60 backdrop-blur shadow p-3 rounded grid gap-5 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 ">
                 {(playlist as SpotifyPlaylist).tracks.items.map(({track}) => { //IDK WHY THE HELL THAT MY IDE YELLS AT ME SO I FORCEFULLY CASTED TYPES
                     return (
                         <div key={track.id}>
@@ -38,13 +38,13 @@ export function TracksGridItem({track}: {track: Track}) {
     return (
         <Link href={`/tracks/${track.id}`}>
             <div className="bg-slate-600/75 hover:bg-slate-500/75 rounded flex p-2 space-x-3 transition-colors duration-300 hover:cursor-pointer">
-                <div className="shrink-0 flex">
+                <div className="shrink-0 flex h-fit w-fit my-auto">
                     <Link href={`albums/${track.album.id}`}>
                         <Image alt={track.album.name + " Album Cover"} src={track.album.images[0].url} width="80" height="80" className="overflow-hidden rounded hover:brightness-90 transition-[filter] duration-300"/>
                     </Link>
                 </div>
                 <div className="flex flex-col min-w-0 hover:min-w-fit hover:z-40">
-                    <div className="font-semibold text-lg truncate hover:text-clip">
+                    <div className="font-semibold text-lg md:truncate hover:text-clip">
                         {track.name}
                     </div>
                     <div className="font-italic text-xs">
