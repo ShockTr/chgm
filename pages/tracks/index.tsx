8c29abbc11ac47112e6acfa76c8fa29b3e1782f3
ShockTr <borakayik@icloud.com>
2022-10-17 00:13:46 +0300

    remove z-index trickery and add titles for TracksGridItem

--- pages/tracks/index.tsx
+++ pages/tracks/index.tsx
@@ -10,7 +10,7 @@ import shimmer from "../../lib/util/shimmer";
 const Tracks = ({ playlist }: {playlist: SpotifyPlaylist}) => {
     return (
         <div className="p-3">
-            <div className="text-white bg-slate-700/60 shadow p-3 rounded grid gap-5 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 ">
+            <div className="text-white bg-slate-800 shadow p-3 rounded grid gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 ">
                 {(playlist).tracks.items.map(({track}) => { //IDK WHY THE HELL THAT MY IDE YELLS AT ME SO I FORCEFULLY CASTED TYPES
                     return (
                         <div className="min-w-0" key={track.id}>
@@ -39,19 +39,19 @@ export default Tracks
 
 export function TracksGridItem({track}: {track: Spotify.Track}) {
     return (
-        <div className="bg-slate-600/75 hover:bg-slate-500/75 rounded p-2 transition-colors duration-300 relative">
+        <div className="bg-slate-700 hover:bg-slate-600 rounded p-2 transition-colors duration-300">
             <div className="flex space-x-3">
-                <Link href={`albums/${track.album.id}`} className="shrink-0 flex h-fit w-fit my-auto z-10">
-                    <Image placeholder="blur" blurDataURL={`data:image/svg+xml;base64,${Buffer.from(shimmer(80, 80)).toString('base64')}`} alt={track.album.name + " Album Cover"} src={track.album.images[0].url} width="80" height="80" className="overflow-hidden rounded hover:brightness-90 transition-[filter] duration-300"/>
+                <Link href={`albums/${track.album.id}`} className="shrink-0 flex h-fit w-fit my-auto">
+                    <Image title={track.album.name} placeholder="blur" blurDataURL={`data:image/svg+xml;base64,${Buffer.from(shimmer(80, 80)).toString('base64')}`} alt={track.album.name + " Album Cover"} src={track.album.images[0].url} width="80" height="80" className="overflow-hidden rounded hover:brightness-90 transition-[filter] duration-300"/>
                 </Link>
                 <div className="flex flex-col min-w-0">
-                    <div className="font-semibold text-lg truncate">
-                        {track.name}
-                    </div>
-                    <div className="font-italic text-xs z-10">
+                    <Link href={`tracks/${track.id}`} className="font-semibold text-lg truncate hover:underline">
+                        <span title={track.name}>{track.name}</span>
+                    </Link>
+                    <div className="font-italic text-xs">
                         by {track.artists.map((artist,index,array) => {
                         return (
-                            <span key={artist.id}>
+                            <span title={artist.name} key={artist.id}>
                                     <Link href={`/artists/${artist.id}`}><span className="hover:cursor-pointer hover:underline">{artist.name}</span></Link>{((array.length - index - 1) !== 0)? ', ': ''}
                             </span>
                         )
@@ -59,7 +59,6 @@ export function TracksGridItem({track}: {track: Spotify.Track}) {
                     </div>
                 </div>
             </div>
-            <Link className="absolute top-0 bottom-0 left-0 right-0 z-0" href={`/tracks/${track.id}`}/>
         </div>
     )
 }
