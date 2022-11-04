b7beb692d08b9f6112cdfd9950d3c9519fc71073
ShockTr <borakayik@icloud.com>
2022-11-04 21:40:45 +0300

    make fetch methods more standardized

--- lib/util/spotify/fetchPlaylist.ts
+++ lib/util/spotify/fetchPlaylist.ts
@@ -1,13 +1,13 @@
 import {Spotify} from "../../../types/spotify";
 import getAccessToken from "./getAccessToken";
 
-export default async function fetchPlaylist () {
-    let {access_token: token} = await getAccessToken()
+export default async function fetchPlaylist (token?:string) {
+    let realToken = token ?? await getAccessToken().then(r => r.access_token)
     let final_playlist: Spotify.PlaylistObjectFull
-    let current_playlist = await fetch(`https://api.spotify.com/v1/playlists/${process.env.PLAYLIST}?market=US`, {headers: {Authorization: `Bearer ${token}`}}).then(r => r.json()) as Spotify.PlaylistObjectFull
+    let current_playlist = await fetch(`https://api.spotify.com/v1/playlists/${process.env.PLAYLIST}?market=US`, {headers: {Authorization: `Bearer ${realToken}`}}).then(r => r.json()) as Spotify.PlaylistObjectFull
     final_playlist = current_playlist
     while (current_playlist.tracks.next !== null) {
-        let next = await fetch(current_playlist.tracks.next, {headers: {Authorization: `Bearer ${token}`}}).then(r => r.json()) as Spotify.PlaylistObjectFull
+        let next = await fetch(current_playlist.tracks.next, {headers: {Authorization: `Bearer ${realToken}`}}).then(r => r.json()) as Spotify.PlaylistObjectFull
         final_playlist.tracks.items.push(...next.tracks.items)
         current_playlist = next
     }
