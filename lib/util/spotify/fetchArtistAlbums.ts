61aafc05940ba6db87855e07de154c933f572996
ShockTr <borakayik@icloud.com>
2022-11-04 22:51:03 +0300

    add include_groups argument to fetchArtistAlbums() function

--- lib/util/spotify/fetchArtistAlbums.ts
+++ lib/util/spotify/fetchArtistAlbums.ts
@@ -3,15 +3,17 @@ import {Spotify} from "../../../types/spotify";
 import ArtistsAlbumsResponse = Spotify.ArtistsAlbumsResponse;
 import AlbumObjectSimplified = Spotify.AlbumObjectSimplified
 
+type groups = "album" | "single" | "appears_on" | "compilation"
 /**
  *
  * @param artistId ID of the artist to fetch
+ * @param include_groups type of albums to be included in the response
  * @param token Provide a token to skip built-in token requester.
  */
-export default async function fetchArtistAlbums(artistId:string, token?:string) : Promise<AlbumObjectSimplified[]>{
+export default async function fetchArtistAlbums(artistId:string, include_groups: groups[], token?:string) : Promise<AlbumObjectSimplified[]>{
     let realToken = token ?? await getAccessToken().then(r => r.access_token)
     let final: AlbumObjectSimplified[]
-    let current = await fetch(`https://api.spotify.com/v1/artists/${artistId}/albums?limit=50&market=US`, {headers: {Authorization: `Bearer ${realToken}`}}).then(r => r.json()) as ArtistsAlbumsResponse
+    let current = await fetch(`https://api.spotify.com/v1/artists/${artistId}/albums?limit=50&market=US&include_groups=${include_groups.join(",")}`, {headers: {Authorization: `Bearer ${realToken}`}}).then(r => r.json()) as ArtistsAlbumsResponse
     final = current.items
     while (current.next !== null) {
         let next = await fetch(current.next, {headers: {Authorization: `Bearer ${realToken}`}}).then(r => r.json()) as ArtistsAlbumsResponse
