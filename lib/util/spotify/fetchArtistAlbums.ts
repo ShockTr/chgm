25333133c1aecf9eafe72e9745c3a4456c9c7fbb
ShockTr <borakayik@icloud.com>
2022-11-05 23:56:36 +0300

    spotify paging helper function

--- lib/util/spotify/fetchArtistAlbums.ts
+++ lib/util/spotify/fetchArtistAlbums.ts
@@ -2,23 +2,16 @@ import getAccessToken from "./getAccessToken";
 import {Spotify} from "../../../types/spotify";
 import ArtistsAlbumsResponse = Spotify.ArtistsAlbumsResponse;
 import AlbumObjectSimplified = Spotify.AlbumObjectSimplified
+import fetchAllPages from "./fetchAllPages";
 
 type groups = "album" | "single" | "appears_on" | "compilation"
 /**
  *
- * @param artistId ID of the artist to fetch
- * @param include_groups type of albums to be included in the response
+ * @param artistId ID of the artist to fetch.
+ * @param include_groups type of albums to be included in the response.
  * @param token Provide a token to skip built-in token requester.
  */
 export default async function fetchArtistAlbums(artistId:string, include_groups: groups[], token?:string) : Promise<AlbumObjectSimplified[]>{
     let realToken = token ?? await getAccessToken().then(r => r.access_token)
-    let final: AlbumObjectSimplified[]
-    let current = await fetch(`https://api.spotify.com/v1/artists/${artistId}/albums?limit=50&market=US&include_groups=${include_groups.join(",")}`, {headers: {Authorization: `Bearer ${realToken}`}}).then(r => r.json()) as ArtistsAlbumsResponse
-    final = current.items
-    while (current.next !== null) {
-        let next = await fetch(current.next, {headers: {Authorization: `Bearer ${realToken}`}}).then(r => r.json()) as ArtistsAlbumsResponse
-        final.push(...next.items)
-        current = next
-    }
-    return final
+    return await fetchAllPages(await fetch(`https://api.spotify.com/v1/artists/${artistId}/albums?limit=50&market=US&include_groups=${include_groups.join(",")}`, {headers: {Authorization: `Bearer ${realToken}`}}).then(r => r.json()) as ArtistsAlbumsResponse, realToken)
 }
