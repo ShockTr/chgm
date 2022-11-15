6b16e0b06f2cc0229d72bcc784936bc34ea2b3b9
ShockTr <borakayik@icloud.com>
2022-11-15 13:24:30 +0300

    new spotify helper functions + type changes to existing functions

--- lib/spotify/fetchArtists.ts
+++ lib/spotify/fetchArtists.ts
@@ -1,8 +1,9 @@
 import getAccessToken from "./getAccessToken";
 import {Spotify} from "../../types/spotify";
+import MultipleArtistsResponse = Spotify.MultipleArtistsResponse;
 
 /**
- * @param ids IDs of the artists to get. max50
+ * @param ids IDs of the artists to fetch. max50
  * @param token Provide a token to skip built-in token requester.
  */
 export default async function fetchArtists (ids: string[], token?:string) {
@@ -10,6 +11,6 @@ export default async function fetchArtists (ids: string[], token?:string) {
     if (ids.length > 50) throw Error("You can only request 50 artists per request.")
     let url = new URL("https://api.spotify.com/v1/artists")
     url.searchParams.set("ids", ids.join(","))
-    let response = await fetch(url, {headers: {Authorization: `Bearer ${realToken}`}}).then(r => r.json()) as {artists: Spotify.ArtistObjectFull[]}
+    let response = await fetch(url, {headers: {Authorization: `Bearer ${realToken}`}}).then(r => r.json()) as MultipleArtistsResponse
     return response.artists
 }
