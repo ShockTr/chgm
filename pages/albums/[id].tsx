afd8ce8845b8cdc7a9cf578078f6a6fb125fe49e
ShockTr <borakayik@icloud.com>
2022-11-16 14:06:10 +0300

    getStaticProps and getStaticPaths for album pages

--- pages/albums/[id].tsx
+++ pages/albums/[id].tsx
@@ -1,17 +1,58 @@
-import {NextPageWithLayout} from "../_app";
 import DefaultLayout from "../../components/layouts/DefaultLayout";
-import {useRouter} from "next/router";
+import {GetStaticPaths, GetStaticProps} from "next";
+import getAccessToken from "../../lib/spotify/getAccessToken";
+import {cacheManager} from "../../lib/cache/fileCache";
+import fetchAllAlbums from "../../lib/spotify/fetchAllAlbums";
+import fetchPlaylist from "../../lib/spotify/fetchPlaylist";
+import {Spotify} from "../../types/spotify";
+import AlbumObjectFull = Spotify.AlbumObjectFull;
+import TrackObjectFull = Spotify.TrackObjectFull;
 
-const Albums: NextPageWithLayout = () => {
-    const router = useRouter()
-    const { id } = router.query
-
+const Albums = ({album, chgmTracks} : {album: AlbumObjectFull, chgmTracks: TrackObjectFull[]}) => {
     return (
         <div className="text-white text-2xl p-5">
-            Album page of: {id}
+            Album page of: {album.name}
         </div>
     )
 }
 
 Albums.getLayout = DefaultLayout
 export default Albums
+
+export const getStaticProps: GetStaticProps = async ({params}) => {
+    let {access_token:token} = await getAccessToken()
+    let allAlbums = cacheManager.getByID("albums")
+    if(!allAlbums) {
+        allAlbums = await fetchAllAlbums(token)
+        cacheManager.setByID("albums", allAlbums)
+    }
+    let album = allAlbums.find(album => album.id === params?.id)
+    let playlist = cacheManager.getByID("playlist")
+    if (!playlist) {
+        playlist = await fetchPlaylist()
+        cacheManager.setByID("playlist", playlist)
+    }
+    let chgmTracks = playlist.tracks.items.map(item => item.track).filter(track => track?.album.id === album?.id)
+    return {
+        props: {
+            album,
+            chgmTracks
+        }
+    }
+}
+
+export const getStaticPaths: GetStaticPaths = async function () {
+    const albums = await fetchAllAlbums()
+    cacheManager.setByID("albums", albums)
+    let paths = albums.map((album) => {
+        return {
+            params: {
+                id: album.id
+            }
+        }
+    })
+    return {
+        paths,
+        fallback: false
+    }
+}
