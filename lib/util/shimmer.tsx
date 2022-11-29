c841957175550ae70eb7bf88d8d46c8c460fe23f
ShockTr <borakayik@icloud.com>
2022-11-29 18:16:51 +0300

    new shimmer element

--- lib/util/shimmer.tsx
+++ lib/util/shimmer.tsx
@@ -13,3 +13,20 @@ export default (w: number, h: number) => `
   <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
   <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"/>
 </svg>`
+
+export function ShimmerElement({w, h}: {w:number, h:number}) {
+    return (
+        <svg width={w} height={h} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+            <defs>
+                <linearGradient id="g">
+                    <stop stopColor="#334155" offset="20%" />
+                    <stop stopColor="#475569" offset="50%" />
+                    <stop stopColor="#334155" offset="70%" />
+                </linearGradient>
+            </defs>
+            <rect width={w} height={h} fill="#334155" />
+            <rect id="r" width={w} height={h} fill="url(#g)" />
+            <animate xlinkHref="#r" attributeName="x" from={-w} to={w} dur="1s" repeatCount="indefinite"/>
+        </svg>
+    )
+}
