e8b9c8a4e1045b90e6f48c5f7ba1ae6caf4e3642
ShockTr <39855554+ShockTr@users.noreply.github.com>
2022-12-24 18:43:03 +0300

    remove the overcomplicated and broken logic in revalidatePlaylist()

--- lib/util/revalidatePlaylist.ts
+++ lib/util/revalidatePlaylist.ts
@@ -1,4 +1,4 @@
-import {Collection, WithId} from "mongodb";
+import {Collection} from "mongodb";
 import {PlaylistData} from "../../types/database";
 import getAccessToken from "../spotify/getAccessToken";
 import fetchPlaylist from "../spotify/fetchPlaylist";
@@ -21,19 +21,16 @@ export async function revalidatePlaylist(collection: Collection<PlaylistData>) {
             })
         }
 
-        let [ biggest ] = await collection.aggregate<WithId<PlaylistData>>([
-            { $sort: {valid_until: -1} },
-            { $limit: 1 }
-        ]).toArray()
-        if (!biggest) await insertNewDoc()
-        else {
-            if (playlist.snapshot_id === biggest.snapshot_id) await collection.updateOne({_id: {$eq: biggest._id}},{
-                $set: {
-                    valid_until: DateTime.fromJSDate(biggest.valid_until ?? new Date()).plus({day: 1}).toJSDate()
-                }
-            })
-            else await insertNewDoc()
-        }
+        let oldObject = await collection.findOne({
+            snapshot_id: {$eq: playlist.snapshot_id}
+        })
+
+        if (oldObject) await collection.updateOne({_id: {$eq: oldObject._id}},{
+            $set: {
+                valid_until: DateTime.fromJSDate(oldObject.valid_until ?? new Date()).plus({day: 1}).toJSDate()
+            }
+        })
+        else await insertNewDoc()
     }
     doc = await collection.findOne({
         valid_until: {$gte: new Date()}
