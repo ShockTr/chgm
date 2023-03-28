21d05a500911e4aaca226f0fce5de59374869f13
ShockTr <39855554+ShockTr@users.noreply.github.com>
2023-03-28 19:02:50 +0300

    audio player package and implementation on TrackList

--- components/TrackList.tsx
+++ components/TrackList.tsx
@@ -3,9 +3,10 @@ import TrackObjectFull = Spotify.TrackObjectFull;
 import TrackObjectSimplified = Spotify.TrackObjectSimplified;
 import shimmer from "../lib/util/shimmer";
 import Image from "next/legacy/image";
-import {useState} from "react";
+import {useRef, useState} from "react";
 import {SpotifyIcon} from "../lib/util/spotifyIcon"
 import Link from "next/link";
+import AudioPlayer from 'react-audio-player';
 
 export function TrackList({tracks, chgmTracks}: { tracks: TrackObjectFull[] | TrackObjectSimplified[], chgmTracks: TrackObjectFull[] }) : JSX.Element {
     return (
@@ -26,18 +27,42 @@ export function TrackList({tracks, chgmTracks}: { tracks: TrackObjectFull[] | Tr
 
 export function TrackListItem({track, index, chgm}: { track: TrackObjectFull | TrackObjectSimplified, index: number, chgm: boolean}) {
     const [hovering, setHover] = useState(false)
+    const [playing, setPlaying] = useState(false)
+    const playerRef = useRef<AudioPlayer>(null)
+    function onClick(){
+        if (playerRef.current) {
+            if (playing) {
+                playerRef.current.audioEl.current?.pause()
+                setPlaying(false)
+            }
+            else {
+                playerRef.current.audioEl.current?.play()
+                setPlaying(true)
+            }
+        }
+    }
     return (
         <div className={`flex h-14 text-white px-3 p-1 rounded justify-between group ${chgm? "bg-gradient-to-r from-sky-900 hover:from-sky-800": "hover:bg-slate-800 "}`} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} >
+            <AudioPlayer volume={0.3} src={track.preview_url as string} ref={playerRef} loop/>
             <div className="flex min-w-0 items-center space-x-2">
                 <div className="w-[18px] shrink-0 text-right group-hover:text-white text-gray-400">
-                    {/*TODO: ADD PREVIEW PLAYER*/}
                     {
                         hovering?
-                            <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-                                    <path d="M7 6v12l10-6z"/>
-                                </svg>
-                            </div>
+                            <button className="flex" onClick={onClick}>
+                                {playing?
+                                    <div>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+                                            <path d="M8 7h3v10H8zm5 0h3v10h-3z"/>
+                                        </svg>
+                                    </div>
+                                    :
+                                    <div>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+                                            <path d="M7 6v12l10-6z"/>
+                                        </svg>
+                                    </div>
+                                }
+                            </button>
                             :
                             (index + 1)
 
