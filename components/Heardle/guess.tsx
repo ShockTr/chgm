10c9497cb2bdb61cfbf84ee9f8920c67b5e964cf
ShockTr <39855554+ShockTr@users.noreply.github.com>
2023-03-28 17:46:43 +0300

    minor touch

--- components/Heardle/guess.tsx
+++ components/Heardle/guess.tsx
@@ -8,7 +8,7 @@ export function HeardleGuess({guess}: {guess?: sotdGuess}){
         </div>
     )
     else if (guess.track === null) return (
-        <div className="flex w-full h-24 border border-slate-700 rounded items-center p-3 space-x-3">
+        <div className="flex w-full h-24 border bg-slate-800 border-slate-700 rounded items-center p-3 space-x-3">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M3 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062A1.125 1.125 0 013 16.81V8.688zM12.75 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062a1.125 1.125 0 01-1.683-.977V8.688z" />
             </svg>
