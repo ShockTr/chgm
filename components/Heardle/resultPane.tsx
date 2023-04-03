990bdc5e73258af5fbd70009ec9f64124c94ebf0
ShockTr <39855554+ShockTr@users.noreply.github.com>
2023-04-03 17:49:55 +0300

    Next game countdown!

--- components/Heardle/resultPane.tsx
+++ components/Heardle/resultPane.tsx
@@ -2,9 +2,26 @@ import { Dialog } from '@headlessui/react'
 import {currentGame, previousSotdGames} from "../../types/sotd";
 import {nodeCrypto, shuffle} from "random-js";
 import {endingMessage, endingMessages, maxGuesses} from "./game";
-import {useMemo} from "react";
+import {useCallback, useEffect, useMemo, useState} from "react";
+import {DateTime} from "luxon";
 
 export function HeardleResultPane({gameState, open, setOpen, previousGames}: {gameState: currentGame, open:boolean, setOpen: (open:boolean) => void, previousGames:previousSotdGames}) {
+    const getRandomEnding = useCallback((type: keyof endingMessage) => {
+        const guessCount = gameState.guesses.length
+        const won = gameState.won
+        let selected: endingMessage
+        if (!won) selected = endingMessages["-1"]
+        else if (endingMessages[guessCount] === undefined) selected = endingMessages[0]
+        else selected = endingMessages[guessCount]
+
+        const shuffled = shuffle(nodeCrypto, selected[type])
+        return shuffled[0]
+    }, [gameState.guesses.length, gameState.won])
+
+    const [countDown, setCountDown] = useState("00:00:00")
+    const date = useMemo(() => DateTime.now().setZone("Asia/Seoul").toISODate(), [])
+    const message = useMemo(() => getRandomEnding("messages"), [getRandomEnding])
+    const title = useMemo(() => getRandomEnding("titles"), [getRandomEnding])
     const values = useMemo(() => Object.values(previousGames), [previousGames])
     const occurrences = useMemo(() => {
         return values.filter((game) => game.won).map((game) => game.guesses).reduce((acc: Record<number, number>, curr) => {
@@ -16,23 +33,30 @@ export function HeardleResultPane({gameState, open, setOpen, previousGames}: {ga
             return acc;
         }, {})
     }, [values])
-    let maxNumberOfGamesWithXGuess = Math.max(...Object.values(occurrences))
     const lost = useMemo(() => values.filter((game) => !game.won).length, [values])
     const won = useMemo(() => values.filter((game) => game.won).length, [values])
-    if (maxNumberOfGamesWithXGuess < lost) maxNumberOfGamesWithXGuess = lost
-
-
-    function getRandomEnding(type: keyof endingMessage){
-        const guessCount = gameState.guesses.length
-        const won = gameState.won
-        let selected: endingMessage
-        if (!won) selected = endingMessages["-1"]
-        else if (endingMessages[guessCount] === undefined) selected = endingMessages[0]
-        else selected = endingMessages[guessCount]
-
-        const shuffled = shuffle(nodeCrypto, selected[type])
-        return shuffled[0]
-    }
+    const maxNumberOfGamesWithXGuess = useMemo(() => {
+        let max = Math.max(...Object.values(occurrences))
+        if (lost > max) max = lost
+        return max
+    }, [lost, occurrences])
+    useEffect(() => {
+        const interval = setInterval(() => {
+            let tomorrow = DateTime.fromISO(date, {zone: "Asia/Seoul"}).plus({days: 1})
+            let now = DateTime.now().setZone("Asia/Seoul")
+            if (tomorrow > now) {
+                let human = tomorrow.diff(now, ["hours", "minutes", "seconds"]).toFormat("hh:mm:ss")
+                setCountDown(human)
+            }
+            else {
+                setCountDown("NOW!")
+                clearInterval(interval)
+            }
+        }, 1000)
+        return () => {
+            clearInterval(interval)
+        }
+    }, [date])
 
     return (
         <Dialog
@@ -46,11 +70,11 @@ export function HeardleResultPane({gameState, open, setOpen, previousGames}: {ga
             {/* Full-screen container to center the panel */}
             <div className="fixed inset-0 flex items-center justify-center p-3">
                 {/* The actual dialog panel  */}
-                <Dialog.Panel className=" text-white max-w-screen-sm w-full h-4/5 rounded bg-slate-800 border border-slate-600 p-3 drop-shadow-md space-y-7">
+                <Dialog.Panel className=" text-white max-w-screen-sm w-full min-h-fit h-2/3 rounded bg-slate-800 border border-slate-600 p-3 drop-shadow-md space-y-7">
                     <div className="space-y-3">
-                        <Dialog.Title className="font-medium text-lg">{getRandomEnding("titles")}</Dialog.Title>
+                        <Dialog.Title className="font-medium text-lg">{title}</Dialog.Title>
                         <Dialog.Description>
-                            {getRandomEnding("messages")}
+                            {message}
                         </Dialog.Description>
                     </div>
                     {/*Stats*/}
@@ -59,10 +83,8 @@ export function HeardleResultPane({gameState, open, setOpen, previousGames}: {ga
                         <div className="flex flex-row w-full justify-between">
                             {
                                 [...Array(gameState.maxGuesses + 1)].map((_, i) => {
-
                                     let count = values.filter((game) => (game.guesses === i + 1) && game.won).length
                                     if (i + 1 === gameState.maxGuesses + 1) count = values.filter((game) => !game.won).length
-
 
                                     return (
                                         <div key={i} className="flex flex-col">
@@ -105,6 +127,15 @@ export function HeardleResultPane({gameState, open, setOpen, previousGames}: {ga
                                     Win Rate
                                 </div>
                             </div>
+                        </div>
+                    </div>
+                    {/*Next game countdown*/}
+                    <div className="flex flex-col items-center justify-center text-lg">
+                        <div className="font-semibold">
+                            Next game in:
+                        </div>
+                        <div>
+                            {countDown}
                         </div>
                     </div>
                 </Dialog.Panel>
