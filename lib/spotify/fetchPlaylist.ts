71fcf6a218501404e3c9e38ac11ed79fa5d88665
ShockTr <39855554+ShockTr@users.noreply.github.com>
2023-05-08 21:43:24 +0300

    fix broken implementation.

--- lib/spotify/fetchPlaylist.ts
+++ lib/spotify/fetchPlaylist.ts
@@ -1,5 +1,6 @@
 import {Spotify} from "../../types/spotify";
 import getAccessToken from "./getAccessToken";
+import fetchAllPages from "./fetchAllPages";
 
 /**
  * @param token Provide a token to skip built-in token requester.
@@ -9,10 +10,6 @@ export default async function fetchPlaylist (token?:string) {
     let final_playlist: Spotify.PlaylistObjectFull
     let current_playlist = await fetch(`https://api.spotify.com/v1/playlists/${process.env.PLAYLIST}?market=US`, {headers: {Authorization: `Bearer ${realToken}`}}).then(r => r.json()) as Spotify.PlaylistObjectFull
     final_playlist = current_playlist
-    while (current_playlist.tracks.next !== null) {
-        let next = await fetch(current_playlist.tracks.next, {headers: {Authorization: `Bearer ${realToken}`}}).then(r => r.json()) as Spotify.PlaylistObjectFull
-        final_playlist.tracks.items.push(...next.tracks.items)
-        current_playlist = next
-    }
+    final_playlist.tracks.items = await fetchAllPages(current_playlist.tracks)
     return final_playlist
 }
